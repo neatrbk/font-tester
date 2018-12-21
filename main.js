db72b6f0c6f46b5bacbec32b5bf86baf00f81f5f
Ken Reiswig <wizard@Kens-MacBook-Air.local>
2018-12-20 22:11:42 -0800

    cleanup

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-const FONTS_API_KEY = 'AIzaSyChIENACvOODK1DcDg41prx1IxZ3EwpP0s';
-const FONTS_API_BASEURL = 'https://www.googleapis.com/webfonts/v1/webfonts?key=';
-
-const fonts = [
-    {fontFamily: "Kumar One Outline", backupFont: "sans-serif", isDisplay: true , order: null, body: null}, 
-    {fontFamily: "Unlock", backupFont: "sans-serif", isDisplay: true, order: null, body: null}, 
-    {fontFamily: "Monoton", backupFont: "sans-serif", isDisplay: true, order: null, body: null},
-    {fontFamily: "Hammersmith One", backupFont: "sans-serif", isDisplay: true, order: null, body: null},
-    {fontFamily: "Shrikhand", backupFont: "sans-serif", isDisplay: true, order: null, body: null},
-    {fontFamily: "Roboto", backupFont: "sans-serif", isDisplay: false, order: null, body: null},
-    {fontFamily: "Montserrat", backupFont: "sans-serif", isDisplay: false, order: null, body: null},
-    {fontFamily: "Slabo 27px", backupFont: "serif", isDisplay: false, order: null, body: null},
-    {fontFamily: "Text Me One", backupFont: "sans-serif", isDisplay: false, order: null, body: null},
-    {fontFamily: "Crimson Text", backupFont: "serif", isDisplay: false, order: null, body: null},
-    {fontFamily: "Arvo", backupFont: "serif", isDisplay: false, order: null, body: null},
-    ];
-
 //let fontData = dataBuilder.createFontData(fonts);
 //
 //async function testRandomization() {
